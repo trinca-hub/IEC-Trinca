c4e430f22e731f6a2d1dc33e2b7c6cc2f9c91fd7
trinca-hub <oliveiratrinca@gmail.com>
2026-08-17 17:43:21 -0300

    Finalizando aula aula 12

--- src/main/java/br/com/fatecads/fatecads/controller/ProdutoController.java
+++ src/main/java/br/com/fatecads/fatecads/controller/ProdutoController.java
@@ -1,13 +1,18 @@
 package br.com.fatecads.fatecads.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import br.com.fatecads.fatecads.entity.Produto;
 import br.com.fatecads.fatecads.service.ProdutoService;
-import java.util.List;
 
 
 @Controller
@@ -23,5 +28,34 @@ public class ProdutoController {
         List<Produto> produtos = produtoService.findAll();
         model.addAttribute("produtos", produtos);
         return "produtos/listarProdutos";
+    }
+
+    //Método para abrir o formulário de criação de produtos  
+    @GetMapping("/criar")
+    public String criarForm(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "produto/formularioProduto";
+    }
+
+    //Método para salvar um produto
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute Produto produto) {
+        produtoService.save(produto);
+        return "redirect:/produtos/listar";
+    }
+
+    //Método para abrir o formulário de edição de produtos 
+    @GetMapping("/editar/{id}")
+    public String editarForm(@PathVariable Integer id, Model model) {
+        Produto produto = produtoService.findById(id);
+        model.addAttribute("produto", produto);
+        return "produto/formulario";
+    }
+
+    //Método para excluir um produto pelo ID
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable Integer id) {
+        produtoService.deleteById(id);
+        return "redirect:/produtos/listar";
     }
 }
